59a43597b4a65fbecf5d5842836bdfea9c8f6d9f
Vex <ly.arv+1@kdru.lol>
2026-06-09 20:46:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vexed",
+    version="2.2",
+    packages=find_packages(),
+    description="The vexed API wrapper",
+    url="https://github.com/vexed-bot/api",
+    author="vexed-bot",
+)
