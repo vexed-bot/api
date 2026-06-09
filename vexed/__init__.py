bc1a3acf4a9970306b6aaadbc82f1dbbeed3a394
Vex <ly.arv+1@kdru.lol>
2026-06-09 21:03:07 +0100

    Update __init__.py

--- vexed/__init__.py
+++ vexed/__init__.py
@@ -1,3 +1,14 @@
 from .vex import *
 
 __version__ = "2.2"
+
+art = r"""
+██    ██ ███████ ██   ██ ███████ ██████  
+██    ██ ██       ██ ██  ██      ██   ██ 
+██    ██ █████     ███   █████   ██   ██ 
+ ██  ██  ██       ██ ██  ██      ██   ██ 
+  ████   ███████ ██   ██ ███████ ██████  
+"""
+
+print(art)
+print(f"Successfully loaded vexed version {__version__}\n")
